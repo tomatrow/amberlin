1a03b54acd562aeb857060d424870901747b8c47
AJ Caldwell <tomatrow@gmail.com>
2025-02-08 15:15:39 -0800

    Prep for demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
 <script lang="ts">
 	import { A } from "$lib/components/A/A.js"
 </script>
-
-<A.CoolList>
-	<A.CoolListItem>Apples</A.CoolListItem>
-	<A.CoolListItem>Apples</A.CoolListItem>
-	<A.CoolListItem>Apples</A.CoolListItem>
-</A.CoolList>
